8c054a152c66da8f2eb7bff3e24d96935f227acf
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2023-03-25 17:52:44 +0500

    changes;

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,18 +6,32 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const getProducts = (path) => (req, res) => {
-  Product.find()
-    .then((products) => {
-      res.render('shop/index', {
-        products,
-        pageTitle: 'Shop',
-        path,
+const ITEMS_PER_PAGE = 1;
 
-        csrfToken: req.csrfToken(),
-      });
-    })
-    .catch(console.log);
+const getProducts = (path) => async (req, res, next) => {
+  const page = +(req.query.page ?? 1);
+
+  try {
+    const totalItems = await Product.find().countDocuments();
+
+    const products = await Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+
+    res.render('shop/index', {
+      products,
+      pageTitle: 'Shop',
+      path,
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getIndex = getProducts('/');
